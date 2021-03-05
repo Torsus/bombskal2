54270eb6436e74662dfc4e89b56c9f81b00786e6
Torsus <crosis767@gmail.com>
2021-03-05 13:21:57 +0100

    c4

--- bombskal2/bombskal2/Program.cs
+++ bombskal2/bombskal2/Program.cs
@@ -11,7 +11,7 @@ namespace bombskal2
     {
         static void Main(string[] args)
         {
-            Process.Start("bomb.exe");/////////
+            Process.Start("bomb.exe");//////////
         }
     }
 }
